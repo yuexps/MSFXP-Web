e1c8181d93659f4f72dbcae295cfd6adb5b0eb9d
yuexps <69074603+yuexps@users.noreply.github.com>
2025-05-23 13:14:34 +0800

    Create hello.js

---
+++ js/hello.js
@@ -0,0 +1,11 @@
+<audio id="Hello_Audio">
+  <source src="./audio/hello.mp3" type="audio/mpeg">
+  您的浏览器不支持音频元素。
+</audio>
+  
+<script>
+  function playAudio() {
+    var x = document.getElementById("Hello_Audio");
+    x.play();
+  }
+</script>
